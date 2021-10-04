a20d125864b33295c20cf8fae28a494e41d03507
Marco <marco.braeuer1984@gmail.com>
2021-10-04 09:23:40 +0200

    neue Leerzeile

--- src/userInterface/TListTester.java
+++ src/userInterface/TListTester.java
@@ -16,6 +16,7 @@ public class TListTester {
 		TAuthorList AuthorList1; // declare
 		AuthorList1 = new TAuthorList(new ArrayList<TAuthor>()); // init
 		AuthorList1.setContent(); // fill list
+		
 		// Test output
 		System.out.println(AuthorList1.get(0).getName()); // output ListObject[0].Name
 		// close connection to database
