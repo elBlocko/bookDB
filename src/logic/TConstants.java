daa324dc75eef0ad11d1077fcd8e7c947c3854a4
Marco <marco.braeuer1984@gmail.com>
2021-10-04 17:21:42 +0200

    Test from Lenovo

--- src/logic/TConstants.java
+++ src/logic/TConstants.java
@@ -3,7 +3,7 @@ package logic;
 public class TConstants {
 	
 	private static String CDatabaseName = "mybooks.db";
-	public static final String CDatabasePath = "C:\\Users\\PC502\\Desktop\\meine\\Database\\" + CDatabaseName;
+	public static final String CDatabasePath = "C:\\Users\\miste\\Desktop\\workspace\\Java\\database\\" + CDatabaseName;
 	
 	
 }
